c05111dca22f00184cd045bc899946e9847b537e
Eries Trisnadi <zdumb1885@outlook.com>
2016-11-16 11:38:59 +0700

    Setting up dependencies and configurations

---
+++ conf/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  /**
+    * Environments.
+    * @var
+    * @param environment {string} - The environment type
+    * Environment list :
+    * - development
+    * - staging
+    * - production
+    */
+  environment: 'development',
+
+  /**
+    * Database Configuration.
+    * @var
+    * @param databaseHost {string} - Your Database Host
+    * @param databaseUsername {string} - Your Database Username
+    * @param databasePassword {string} - Your Database Password
+    * @param databaseName {string} - Your Database Name
+    */
+
+  // databaseHost      : 'localhost',
+  // databaseUsername  : 'user',
+  // databasePassword  : 'pass',
+  // databaseName      : 'database_name'
+
+  databaseHost      : 'localhost',
+  databaseUsername  : 'onlineak',
+  databasePassword  : 'indo12soft345',
+  databaseName      : 'Sorairo_Fansub'
+
+
+}
