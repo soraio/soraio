c6e22939b164cb0deea2a5bd12b8987396bf2c77
Eries Trisnadi <zdumb1885@outlook.com>
2016-11-16 11:45:03 +0700

    create database middleware with bookshelf ORM and knexjs

---
+++ core/db/database.js
@@ -0,0 +1,14 @@
+/**
+  * Setting up database uses bookshelf ORM.
+  */
+var config    = require('../../conf/config'),
+
+    // Selects the correct DB config object for the current environment.
+    knex      = require('knex')(require('../../knexfile')[config.environment]),
+    bookshelf = require('bookshelf')(knex)
+
+    // Resolve circular dependencies with relations.
+    bookshelf.plugin('registry')
+    bookshelf.plugin(require('bookshelf-modelbase').pluggable)
+
+module.exports = bookshelf
