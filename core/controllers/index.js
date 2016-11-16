ab9deacd873972578be27a2428d6266da77656bc
Eries Trisnadi <zdumb1885@outlook.com>
2016-11-16 11:52:01 +0700

    create IndexController

---
+++ core/controllers/index.js
@@ -0,0 +1,40 @@
+/**
+  * Module dependencies.
+  */
+var express = require('express'),
+    IndexController = express.Router(),
+    // include Post model
+    Post = require('../models/post')
+
+/**
+  * GET / rules.
+  */
+IndexController.route('/')
+.get(function(req, res, next) {
+  Post.fetchAll({withRelated: ['user'], require: true})
+  .then(function(posts){
+    res.render('index', {posts: posts.toJSON()})
+  })
+  .catch(function(err) {
+    next()
+  })
+})
+
+/**
+  * GET /posts/pid rules.
+  * @param pid {post_id}.
+  */
+IndexController.route('/posts/:pid')
+.get(function(req, res, next) {
+  var pid = req.params.pid
+  Post.where({id: pid})
+  .fetch({withRelated: ['user'], require: true})
+  .then(function(post){
+    res.json(post)
+  })
+  .catch(function(err) {
+    next()
+  })
+})
+
+module.exports = IndexController
