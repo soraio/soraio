32275e2e206ea7c6f56748dc4c1a15c5cde4ac62
Eries Trisnadi <zdumb1885@outlook.com>
2016-11-22 14:45:45 +0700

    update routes to more secure for backend

--- core/routes.js
+++ core/routes.js
@@ -17,10 +17,23 @@ route.locals.moment = require('moment')
 /**
   * Routing the controllers.
   */
+// Public section
 route.use('/', IndexController)
+
+// Secure section, needs authenticated user to access this rules
+route.use('/backend[\/]?*', ensureAuthenticated)
 
 // Secure section, needs csrftoken to access this rules
 route.use(csrf({ cookie: true }))
 route.use('/auth', AuthController)
 
+/**
+  * Function to ensure if the client is authenticated
+  */
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated())
+    return next()
+  else
+    res.redirect('/auth/login')
+}
 module.exports = route
