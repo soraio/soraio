c2a20b98f52f1a8842d2ba8f7c656aa08db8e831
Eries Trisnadi <zdumb1885@outlook.com>
2016-11-23 16:23:20 +0700

    add GET /backend/posts/all|user

--- core/controllers/posts.js
+++ core/controllers/posts.js
@@ -2,7 +2,9 @@
   * Module dependencies.
   */
 var express = require('express'),
-    PostsController = express.Router()
+    PostsController = express.Router(),
+    // include Post model
+    Post = require('../models/post')
 
 /**
   * GET /backend/posts/add rules.
@@ -10,6 +12,28 @@ var express = require('express'),
 PostsController.route('/add')
 .get(function(req, res, next) {
   res.render('posts/add', {user: req.user})
+})
+
+PostsController.route('/user')
+.get(function(req, res, next) {
+  Post.where({user_id: req.user.id}).fetchAll({withRelated: ['user'], require: true})
+  .then(function(posts){
+    res.render('posts/posts', {user: req.user, posts: posts.toJSON()})
+  })
+  .catch(function(err) {
+    next()
+  })
+})
+
+PostsController.route('/all')
+.get(function(req, res, next) {
+  Post.fetchAll({withRelated: ['user'], require: true})
+  .then(function(posts){
+    res.render('posts/posts', {user: req.user, posts: posts.toJSON()})
+  })
+  .catch(function(err) {
+    next()
+  })
 })
 
 module.exports = PostsController
