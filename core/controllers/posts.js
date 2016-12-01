1d25c4c510d9851ea1c95e7d793ab808560e9367
Eries Trisnadi <zdumb1885@outlook.com>
2016-12-01 08:49:08 +0700

    update posts controller withrelated project

--- core/controllers/posts.js
+++ core/controllers/posts.js
@@ -4,30 +4,38 @@
 var express = require('express'),
     PostsController = express.Router(),
     // include Post model
-    Post = require('../models/post')
+    Post = require('../models/post'),
+    Project = require('../models/project')
 
 /**
   * GET /backend/posts/add rules.
   */
 PostsController.route('/add')
 .get(function(req, res, next) {
-  res.render('posts/add', {user: req.user})
-})
-
-PostsController.route('/user')
-.get(function(req, res, next) {
-  Post.where({user_id: req.user.id}).fetchAll({withRelated: ['user'], require: true})
-  .then(function(posts){
-    res.render('posts/posts', {user: req.user, posts: posts.toJSON()})
+  Project.fetchAll()
+  .then(function(projects) {
+    res.render('posts/add', {user: req.user, projects: projects.toJSON()})
   })
   .catch(function(err) {
     next()
   })
 })
 
+PostsController.route('/user')
+.get(function(req, res, next) {
+  Post.where({user_id: req.user.id}).fetchAll({withRelated: ['user', 'project'], require: true})
+  .then(function(posts){
+    res.render('posts/posts', {user: req.user, posts: posts.toJSON()})
+  })
+  .catch(function(err) {
+    console.log(err)
+    next()
+  })
+})
+
 PostsController.route('/all')
 .get(function(req, res, next) {
-  Post.fetchAll({withRelated: ['user'], require: true})
+  Post.fetchAll({withRelated: ['user', 'project'], require: true})
   .then(function(posts){
     res.render('posts/posts', {user: req.user, posts: posts.toJSON()})
   })
