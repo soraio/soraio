042c40690442bad92098f04ab71a50d2ae872dcb
Eries Trisnadi <zdumb1885@outlook.com>
2017-02-18 20:11:27 +0700

    update test file, add mocha-jshit

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
-var request = require('supertest')
-var should = require('should')
-var app = require('../lib/server')
-
+var request = require('supertest'),
+    should = require('should'),
+    app = require('../lib/server')
+    require('mocha-jshint')({
+      paths: [
+        'lib',
+        'core'
+      ],
+      pretty: true
+    })
 describe('GET / index | GET / params', function() {
   it('should respond with json', function(done) {
     request(app)
@@ -15,9 +21,9 @@ describe('GET / index | GET / params', function() {
       done()
     })
   })
-  it('should respond with status 200 OK if got specific Post ID', function(done) {
+  it('should respond with status 200 OK if got specific Post', function(done) {
     request(app)
-    .get('/posts/1')
+    .get('/posts/hi-no-youjin-movie')
     .set('Accept', 'application/json')
     .expect(200)
     .end(function(err, res) {
@@ -27,7 +33,7 @@ describe('GET / index | GET / params', function() {
       done()
     })
   })
-  it('should respond with status 404 Not Found when got no specific Post ID', function(done) {
+  it('should respond with status 404 Not Found when got no specific Post', function(done) {
     request(app)
     .get('/posts/11283182931231')
     .set('Accept', 'application/json')
@@ -39,9 +45,21 @@ describe('GET / index | GET / params', function() {
       done()
     })
   })
+  it('should respond with status 302 Found when anonymous try to access admin sections', function(done) {
+    request(app)
+    .get('/admin')
+    .set('Accept', 'application/json')
+    .expect(302)
+    .end(function(err, res) {
+      if (err) throw err
+
+      res.should.have.property('status', 302)
+      done()
+    })
+  })
   it('should respond with status 404 Not Found when got no registered routes', function(done) {
     request(app)
-    .get('/admin/abc/')
+    .get('/asdbas/asdas/asd')
     .set('Accept', 'application/json')
     .expect(404)
     .end(function(err, res) {
